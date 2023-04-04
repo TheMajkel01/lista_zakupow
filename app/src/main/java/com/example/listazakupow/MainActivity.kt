4343a5eaf4383e9ddb7554365be44198243f6031
TheMajkel01 <michalnl@onet.pl>
2023-04-05 00:05:58 +0200

    Starting screen base changed

--- app/src/main/java/com/example/listazakupow/MainActivity.kt
+++ app/src/main/java/com/example/listazakupow/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.listazakupow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var addProduct = findViewById<LinearLayout>(R.id.add)
+        var productList = findViewById<LinearLayout>(R.id.list)
 
+        addProduct.visibility = View.GONE
+        productList.visibility = View.GONE
     }
 }
