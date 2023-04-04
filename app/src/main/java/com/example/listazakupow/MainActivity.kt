6f52408fbb460d1ce24f1920a88dd8c20bf51255
TheMajkel01 <michalnl@onet.pl>
2023-04-05 00:39:18 +0200

    Switching modes with a radio group kind of done

--- app/src/main/java/com/example/listazakupow/MainActivity.kt
+++ app/src/main/java/com/example/listazakupow/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.RadioButton
+import android.widget.RadioGroup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +14,23 @@ class MainActivity : AppCompatActivity() {
 
         var addProduct = findViewById<LinearLayout>(R.id.add)
         var productList = findViewById<LinearLayout>(R.id.list)
+        
+        var radioGroup = findViewById<RadioGroup>(R.id.choiceGroup)
 
         addProduct.visibility = View.GONE
         productList.visibility = View.GONE
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.radioButton -> {
+                    productList.visibility = View.GONE
+                    addProduct.visibility = View.VISIBLE
+                }
+                R.id.radioButton2 -> {
+                    addProduct.visibility = View.GONE
+                    productList.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
